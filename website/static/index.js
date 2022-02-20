3aedccdeca1eaf9c9c2e1b03dbda0ff8555ba495
Curtis <sy3135@nyu.edu>
2022-02-20 18:10:22 -0500

    add API service, cool cat co2e page

--- website/static/index.js
+++ website/static/index.js
@@ -1,71 +1,21 @@
 
-    var $tbl = $('#gridTable1');
-    var mydata =[
-      {
-        "token_address": "0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d",
-        "from_address": "0x752f4ab1cb31201c0665f11434dee3835189c088",
-        "to_address": "0xe96d8a0ef27285194f26def5584f30a926ad492f",
-        "value": "8880",
-        "transaction_hash": "0xb30d9ed5ff4e6ab8f1cb721108148a49898b4e7176fcab52b1361af5244ce936",
-        "log_index": "130",
-        "block_timestamp": "2022-01-14 11:52:22 UTC",
-        "block_number": "14003476",
-        "block_hash": "0xf1ebc9380e79754187840d77e62d916948941fb6643bfb430ba85070a5e657b0",
-        "hash": "0xb30d9ed5ff4e6ab8f1cb721108148a49898b4e7176fcab52b1361af5244ce936",
-        "nonce": "263",
-        "transaction_index": "90",
-        "from_address_1": "0x752f4ab1cb31201c0665f11434dee3835189c088",
-        "to_address_1": "0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d",
-        "value_1": "0",
-        "gas": "124599",
-        "gas_price": "133641706488",
-        "input": "0x23b872dd000000000000000000000000752f4ab1cb31201c0665f11434dee3835189c088000000000000000000000000e96d8a0ef27285194f26def5584f30a926ad492f00000000000000000000000000000000000000000000000000000000000022b0",
-        "receipt_cumulative_gas_used": "5957237",
-        "receipt_gas_used": "114999",
-        "receipt_contract_address": null,
-        "receipt_root": null,
-        "receipt_status": "1",
-        "block_timestamp_1": "2022-01-14 11:52:22 UTC",
-        "block_number_1": "14003476",
-        "block_hash_1": "0xf1ebc9380e79754187840d77e62d916948941fb6643bfb430ba85070a5e657b0",
-        "max_fee_per_gas": "201591565623",
-        "max_priority_fee_per_gas": "1500000000",
-        "transaction_type": "2",
-        "receipt_effective_gas_price": "133641706488"
+function postData(url, data) {
+  // Default options are marked with *
+  return fetch(url, {
+      body: JSON.stringify(data), // must match 'Content-Type' header
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, same-origin, *omit
+      headers: {
+          'user-agent': 'Example',
+          'content-type': 'application/json'
       },
-      {
-        "token_address": "0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d",
-        "from_address": "0x721931508df2764fd4f70c53da646cb8aed16ace",
-        "to_address": "0x7405fe24003a50e4f4117d35e9b5a9f5e512fede",
-        "value": "4583",
-        "transaction_hash": "0x21210e30676fcb2131c23a469e49db10ebb9f7352eab6f4b629135ad7db63f09",
-        "log_index": "60",
-        "block_timestamp": "2021-05-31 02:29:27 UTC",
-        "block_number": "12539478",
-        "block_hash": "0x95fef85fefd53b80c42311c3fc38a6395cb719226564e0b0c6f1f5ad5d96e5d0",
-        "hash": "0x21210e30676fcb2131c23a469e49db10ebb9f7352eab6f4b629135ad7db63f09",
-        "nonce": "11605",
-        "transaction_index": "33",
-        "from_address_1": "0x721931508df2764fd4f70c53da646cb8aed16ace",
-        "to_address_1": "0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d",
-        "value_1": "0",
-        "gas": "117287",
-        "gas_price": "21000000000",
-        "input": "0x23b872dd000000000000000000000000721931508df2764fd4f70c53da646cb8aed16ace0000000000000000000000007405fe24003a50e4f4117d35e9b5a9f5e512fede00000000000000000000000000000000000000000000000000000000000011e7",
-        "receipt_cumulative_gas_used": "2208240",
-        "receipt_gas_used": "87287",
-        "receipt_contract_address": null,
-        "receipt_root": null,
-        "receipt_status": "1",
-        "block_timestamp_1": "2021-05-31 02:29:27 UTC",
-        "block_number_1": "12539478",
-        "block_hash_1": "0x95fef85fefd53b80c42311c3fc38a6395cb719226564e0b0c6f1f5ad5d96e5d0",
-        "max_fee_per_gas": null,
-        "max_priority_fee_per_gas": null,
-        "transaction_type": null,
-        "receipt_effective_gas_price": "21000000000"
-      }
-    ]
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, cors, *same-origin
+      redirect: 'follow', // manual, *follow, error
+      referrer: 'no-referrer', // *client, no-referrer
+  })
+      .then(response => response.json()) // output json
+}
     
     function showtable()
     {
@@ -76,3 +26,8 @@
             $("#btn_search").click(function () {
                 showtable()
             });
+
+    function submit(){
+
+      console.log("ok")
+    }
