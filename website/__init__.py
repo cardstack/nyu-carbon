319392135e7c9178a696922d73a86082bab9cd2a
Curtis <sy3135@nyu.edu>
2022-02-28 02:53:25 -0500

    add requirement3

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
+from boto.s3.connection import S3Connection
 import os
 
 db = SQLAlchemy()
@@ -10,7 +11,8 @@ db_pass = os.environ.get('DB_PASS')
 def create_app():
     app =Flask(__name__)
     app.config['SECRET_KEY'] = 'nyu-carbon'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://' + str(db_user) +':'+ str(db_pass) +'@nnsgluut5mye50or.cbetxkdyhwsb.us-east-1.rds.amazonaws.com/re4nl1r93wwgmzuc'
+    #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://' + str(db_user) +':'+ str(db_pass) +'@nnsgluut5mye50or.cbetxkdyhwsb.us-east-1.rds.amazonaws.com/re4nl1r93wwgmzuc'
+    app.config['SQLALCHEMY_DATABASE_URI']= os.environ['JAWSDB_URL']
     db.init_app(app)
     CORS(app)
 
