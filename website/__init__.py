e27fd70d57e30b6709fd65dfd2232a128d154a8d
Curtis <sy3135@nyu.edu>
2022-02-28 14:54:18 -0500

    config 6

--- website/__init__.py
+++ website/__init__.py
@@ -11,7 +11,7 @@ db_url = os.environ.get('DB_URL')
 def create_app():
     app =Flask(__name__)
     app.config['SECRET_KEY'] = 'nyu-carbon'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://f61oe34ieryulbh6:oqj8v5d5y37ituc4@nnsgluut5mye50or.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/re4nl1r93wwgmzuc'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://f61oe34ieryulbh6:subsp5uly1fd6ljl@nnsgluut5mye50or.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/re4nl1r93wwgmzuc'
     #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://' + str(db_user) +':'+ str(db_pass) +'@nnsgluut5mye50or.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/re4nl1r93wwgmzuc'
     #app.config['SQLALCHEMY_DATABASE_URI']= 'mysql+pymysql://' + str(db_url)
     db.init_app(app)
