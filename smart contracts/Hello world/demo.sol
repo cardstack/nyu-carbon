04941a89647dc15cc64e45a3b368bf92d5673fa2
curtisyang2008 <curtisyang2011@gmail.com>
2022-03-09 18:31:51 -0500

    Create demo.sol

---
+++ smart contracts/Hello world/demo.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+
+contract DemoContract {
+    struct Transcation {
+        string contract_address;
+        uint token_id;
+        uint co2;
+    }
+
+    Transcation[] public transcations;
+    
+    function fetchTransaction(string memory contract_address, uint token_id, uint co2) public{
+        transcations.push(Transcation(contract_address, token_id, co2));
+    }  
+
+    function getAll() view public returns(Transcation[] memory) {
+    return transcations;
+    }
+
+    function getToken(uint position) view public returns(uint) {
+    return transcations[position].co2/10;
+    }
+
+    function destroy(uint position) public {
+        delete transcations[position];
+    }
+}
