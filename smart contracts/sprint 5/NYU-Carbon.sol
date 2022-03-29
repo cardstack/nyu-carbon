2ab934ff178d635b95c275ce6b9943579a2fd6b9
Curtis <sy3135@nyu.edu>
2022-03-29 07:28:20 -0400

    sprint 5 ver1

--- smart contracts/sprint 5/NYU-Carbon.sol
+++ smart contracts/sprint 5/NYU-Carbon.sol
@@ -1,24 +1,35 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 <0.9.0;
 
-contract NYUCarbonContract {
-    mapping(address => mapping(uint256 => uint256)) private offsets;
-    event token_offset(address contract_address, uint256 id, uint256 amount);
-    uint256 BCT= 1000;
-    
-    function offset_nft(address contract_address, uint256 token_id, uint256 amt) public{
-        
-        offsets[contract_address][token_id] += amt;
-        BCT = BCT - amt;
-        emit token_offset(contract_address, token_id, amt);
-    }  
+import 'BCTContract.sol';
 
-    function getBalance() view public returns(uint) {
-        return BCT;
+contract NYUCarbonContract {
+
+    mapping(address => mapping(uint256 => uint256)) private offsets;
+    event OffsetNFT(address indexed sender, uint amount);
+    tokenBCT token = tokenBCT(0xCb38eC62A43EB019F8d79b0C1C070ccdD16D1650);
+    
+    function getBCTBalance() view public returns(uint){
+        return token.balanceOf(address(this));
     }
+ 
+    function offsetNFT(address contract_address, uint256 token_id, uint256 amt) public { 
+        token.transferFrom(msg.sender, address(this), amt);
+        offsets[contract_address][token_id] += amt;
+        emit OffsetNFT(msg.sender, amt);
+        
+    }  
 
     function getOffset(address contract_address, uint256 token_id) view public returns(uint) {
         return  offsets[contract_address][token_id];
     }
+
+    // burn all balance in the contract
+    function burnToken() public payable {
+        token.burn(token.balanceOf(address(this)));
+        
+    }
+
+
 
 }
