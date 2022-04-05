8f88e52b1d5d2c0fb667d529704162390430d704
Curtis <sy3135@nyu.edu>
2022-04-05 07:20:28 -0400

    sprint 6 complete

--- smart contracts/sprint 6/NYUcontractV2.sol
+++ smart contracts/sprint 6/NYUcontractV2.sol
@@ -5,50 +5,31 @@ import 'BCTContract.sol';
 
 contract NYUCarbonContract {
     address owner;
-    bool flag_burnt;
     uint256 amount;
+    tokenBCT token;
     mapping(address => mapping(uint256 => uint256)) private offsets;
-    mapping(address => mapping(uint256 => uint256)) private green_status;
+    mapping(address => mapping(uint256 => string)) private green_status;
 
+    // events that will be emited
     event burnt(address indexed sender, uint amount, uint256 time);
-    event debit(address contract_address, uint256 amount, uint256 time);
-
+    event debit(address sender_address, address contract_address, uint256 amount, uint256 time);
+    event carbon_free(address contract_address, uint256 amount, uint256 time);
     
+    // State variable to ensure funtions are called ordinally: Pending -> Locked -> Burnt, prevent duplicate calls
+    enum State {Pending, Locked, Burnt}
+    State public state;
+    error InvalidState();
 
     constructor() {
         owner = msg.sender;
-        flag_burnt = false;
         amount = 0;
-        tokenBCT token = tokenBCT(0xCb38eC62A43EB019F8d79b0C1C070ccdD16D1650);
+        token = tokenBCT(0xCb38eC62A43EB019F8d79b0C1C070ccdD16D1650);
     }
 
-    function getBCTBalance() view public returns(uint){
-        return token.balanceOf(address(this));
-    }
-
-    function getTime() public view returns(uint256) {
-        return block.timestamp;
-    }
-
-    function getOffset(address contract_address, uint256 token_id) view external returns(uint) {
-        return offsets[contract_address][token_id];
-    }
-
-    // user debit ERC20 tokens to the contract for burning
-    function offsetNFT(uint256 amt) external { 
-        token.transferFrom(msg.sender, address(this), amt);
-        emit debit(address(this), amt, getTime());
-        amount += amt;
-    }
-
-    // burn all balance in the contract, only the owner can burn the token
-    function burnToken() public isOwner {
-        // burn the balance
-        require(getBCTBalance() > 0, "Not enough balance");
-        token.burn(token.balanceOf(address(this)));
-        // emit offset activity once burnt confirmed
-        emit burnt(msg.sender, amount, getTime());
-        flag_burnt = true;   
+    modifier inState(State _state) {
+        if (state != _state)
+            revert InvalidState();
+        _;
     }
 
     modifier isOwner{
@@ -56,18 +37,61 @@ contract NYUCarbonContract {
         _;
     }
 
+    function getBCTBalance() view public returns(uint){
+        return token.balanceOf(address(this));
+        
+    }
+
+    function BalanceOfSender() view public returns(uint){
+        return token.balanceOf(msg.sender);
+    }
+
+    function getTime() view public returns(uint256) {
+        return block.timestamp;
+    }
+
+    function getOffset(address contract_address, uint256 token_id) view public returns(uint) {
+        return offsets[contract_address][token_id];
+    }
+
+    // user debit ERC20 tokens to the contract for burning
+    function offsetNFT(address contract_address, uint256 token_id, uint256 amt) external inState(State.Pending) { 
+        token.transferFrom(msg.sender, address(this), amt);
+        emit debit(msg.sender, address(this), amt, getTime());
+        offsets[contract_address][token_id] += amt;
+        //amount += amt;
+        
+
+        // after receiving the fund, lock them in the contract
+        state = State.Locked;
+        burnToken(amt);
+        stamping(contract_address, token_id, amt);
+    }
+
+    // burn all balance in the contract, only this contract can burn the token
+    function burnToken(uint amt) public isOwner inState(State.Locked) {
+        // burn the balance
+        
+        require(getBCTBalance() > 0, "Not enough balance");
+        token.burn(token.balanceOf(address(this)));
+        
+        // emit offset activity once burnt confirmed
+        emit burnt(msg.sender, amt, getTime());
+        // after burning success, change the state to burnt
+        state = State.Burnt;  
+    }
+
     // funciton of record offset amount and fake function of producing a green stamp
-    function stamping(uint256 CO2_balance, address contract_address, uint256 token_id) public returns(bool) {
-        // record the offset amounts
-        require(flag_burnt == true, "please burn the token first");
-        offsets[contract_address][token_id] += amount;
+    function stamping(address contract_address, uint256 token_id, uint256 CO2_balance) public inState(State.Burnt) {
 
         // fake some balance, just for functionality check
-        CO2_balance = getOffset(contract_address, token_id);
-        if(full_offset = CO2_balance - getOffset(contract_address, token_id) == 0) {
+        if((getOffset(contract_address, token_id) - CO2_balance) >= 0) {
             green_status[contract_address][token_id] = "Carbon Free";
+            emit carbon_free(contract_address, token_id, getTime());
         }else{
             green_status[contract_address][token_id] = "Not Carbon Free";
         }
+        // all process finished, change the state back to pending, ready for another offset
+        state = State.Pending;
     }
 }
